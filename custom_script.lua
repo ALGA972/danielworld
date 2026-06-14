4ff8f970cd1290018a0c0f2e374c7d29995d53ad
ALGA972 <gustavitoolavarria@gmail.com>
2026-06-14 18:20:45 -0400

    Create custom script file

---
+++ custom_script.lua
@@ -0,0 +1,5 @@
+-- custom_script.lua
+-- Archivo limpio para que pegues tu script Lua.
+-- Úsalo solo en tu propia experiencia/juego de Roblox Studio.
+
+print("custom_script.lua cargado correctamente")
